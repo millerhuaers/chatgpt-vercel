91e1948b4dd0d084e2debd8e91f93beadbe91bf9
millerhuaers <millerhuaers185@gmail.com>
2024-11-04 17:26:04 +0800

    Update types.ts

--- shared/types.ts
+++ shared/types.ts
@@ -13,8 +13,8 @@ export interface ChatMessage {
 }
 
 export type Role = "system" | "user" | "assistant" | "error"
-export type SimpleModel = "gpt-4o” | "gpt-4o-mini” | "gpt-4-turbo-2024-04-09” | "claude-3-haiku-20240307” | "claude-3-5-sonnet-20240620” | "gemini-1.5-pro-latest” | "gemini-1.5-flash-latest” | "Meta-Llama-3.1-70B-Instruct-Turbo” | "Meta-Llama-3.1-405B-Instruct-Turbo” | "llama-3.1-sonar-large-128k-online” | "mistral-large-2407”
-export type Model = "gpt-4o” | "gpt-4o-mini” | "gpt-4-turbo-2024-04-09” | "claude-3-haiku-20240307” | "claude-3-5-sonnet-20240620” | "gemini-1.5-pro-latest” | "gemini-1.5-flash-latest” | "Meta-Llama-3.1-70B-Instruct-Turbo” | "Meta-Llama-3.1-405B-Instruct-Turbo” | "llama-3.1-sonar-large-128k-online” | "mistral-large-2407”
+export type SimpleModel = "gpt-4o" | "gpt-4o-mini" | "gpt-4-turbo-2024-04-09" | "claude-3-haiku-20240307" | "claude-3-5-sonnet-20240620" | "gemini-1.5-pro-latest" | "gemini-1.5-flash-latest" | "Meta-Llama-3.1-70B-Instruct-Turbo" | "Meta-Llama-3.1-405B-Instruct-Turbo" | "llama-3.1-sonar-large-128k-online" | "mistral-large-2407"
+export type Model = "gpt-4o" | "gpt-4o-mini" | "gpt-4-turbo-2024-04-09" | "claude-3-haiku-20240307" | "claude-3-5-sonnet-20240620" | "gemini-1.5-pro-latest" | "gemini-1.5-flash-latest" | "Meta-Llama-3.1-70B-Instruct-Turbo" | "Meta-Llama-3.1-405B-Instruct-Turbo" | "llama-3.1-sonar-large-128k-online" | "mistral-large-2407"
 
 export interface Prompt {
   desc: string
